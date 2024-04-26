ff3eea60beeafdbaa20cfba5e69baef91e748f63
Jeyker-Dev <mendozajeyker1178@gmail.com>
2024-04-26 11:01:15 -0400

    Creada Funcion para Comprobar Rutas o En Router

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController
+{
+
+    public function __construct()
+    {
+
+    }   // Here End Construct
+
+    public function index()
+    {
+
+    }   // Here End Function
+}   // Here End Class
+
+?>
