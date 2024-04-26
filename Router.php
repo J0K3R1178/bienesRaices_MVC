1682a9174c3d50b04fbd1a5a4cfcd4c0ff80370d
Jeyker-Dev <mendozajeyker1178@gmail.com>
2024-04-26 11:04:40 -0400

    Agregando Rutas y Comprobandolas

--- Router.php
+++ Router.php
@@ -1,11 +1,43 @@
 <?php
 
+namespace MVC;
+
 class Router
 {
+    public $rutas_get = [];
+    public $rutas_post = [];
+
     public function __construct()
     {
-
+        
     }   // Here End Construct
+
+    public function comprobarRutas()
+    {
+        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if($metodo == 'GET')
+        {
+            $funcion = $this->rutas_get[$url_actual] ?? null;
+        }   // Here End If
+
+        if($funcion)
+        {
+            // Call a Function That is Unknown Until Execution Time
+            call_user_func( $funcion, $this);
+        }   // Here End If
+        else
+        {
+            echo "Pagina No Encontrada";
+        }   // Here End Else
+
+    }   // Here End Function
+
+    public function get($url, $funcion)
+    {
+        $this->rutas_get[$url] = $funcion;
+    }   // Here End Function
 
 }   // Here End Class
 
--- public/index.php
+++ public/index.php
@@ -1,8 +1,27 @@
 <?php
 
+// USES
+use MVC\Router;
+use Controllers\PropiedadController;
+
+//  APP
 require_once __DIR__ . "/../includes/app.php";
 
+// Header
 incluirTemplate('header');
+
+// Instance variables
+$router = new Router();
+
+$router->get('/', 'funcion_index');
+$router->get('/admin/', 'funcion_admin');
+$router->get('/propiedades/crear', 'funcion_propiedades_crear');
+$router->get('/propiedades/actualizar', 'funcion_propiedades_actualizar');
+$router->get('/propiedades/crear', 'funcion_propiedades_crear');
+
+
+
+$router->comprobarRutas();
 
 ?>
 
